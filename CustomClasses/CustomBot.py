9cae40b55ed3d978de77339726a099e5a664a006
matthew <matthewanderson3607@gmail.com>
2022-10-16 09:40:16 -0500

    test

--- CustomClasses/CustomBot.py
+++ CustomClasses/CustomBot.py
@@ -424,7 +424,6 @@ class CustomClient(commands.Bot):
         except:
             return None
 
-    #THIS IS MASTER
     #SERVER HELPERS
     async def open_clan_capital_reminders(self):
         pass
