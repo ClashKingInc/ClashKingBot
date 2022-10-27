c7732f9297315ede1c55a85511353721f7575bda
matthew <matthewanderson3607@gmail.com>
2022-10-26 20:15:57 -0500

    what is done on reminders thus far

--- BackgroundCrons/reminders.py
+++ BackgroundCrons/reminders.py
@@ -11,19 +11,36 @@ class reminders(commands.Cog):
     def __init__(self, bot: CustomClient):
         self.bot = bot
         #ends at 7 am monday
-        scheduler.add_job(self.clan_capital_reminder, "cron", args=["1 hr"], day_of_week="mon", hour=6)
-        scheduler.add_job(self.clan_capital_reminder, "cron", args=["12 hr"], day_of_week="sun", hour=19)
-        scheduler.add_job(self.clan_capital_reminder, "cron", args=["24 hr"], day_of_week="sun", hour=7)
+        #scheduler.add_job(self.clan_capital_reminder, "cron", args=["1 hr"], day_of_week="mon", hour=6)
+        #scheduler.add_job(self.clan_capital_reminder, "cron", args=["6 hr"], day_of_week="mon", hour=1)
+        #scheduler.add_job(self.clan_capital_reminder, "cron", args=["12 hr"], day_of_week="sun", hour=19)
+        #scheduler.add_job(self.clan_capital_reminder, "cron", args=["24 hr"], day_of_week="sun", hour=7)
 
-    @commands.slash_command(name="reminder")
+
+    @commands.slash_command(name="reminders")
     async def reminder(self, ctx):
         pass
 
+    async def clan_converter(self, clan_tag: str):
+        clan = await self.bot.getClan(clan_tag=clan_tag, raise_exceptions=True)
+        if clan.member_count == 0:
+            raise coc.errors.NotFound
+        return clan
+
     @reminder.sub_command(name="create", description="Set a reminder for clan games, raid weekend, wars, & more")
-    async def reminder_create(self, ctx: disnake.ApplicationCommandInteraction, clan:str, type:str = commands.Param(choices=["Clan Capital"])):
-        clan = await self.bot.getClan(clan_tag=clan)
-        if clan is None:
-            return await ctx.send("Not a valid clan tag")
+    async def reminder_create(self, ctx: disnake.ApplicationCommandInteraction, channel: disnake.TextChannel, type:str = commands.Param(choices=["Clan Capital", "Clan Games" , "War", "Inactivity"]), clan: coc.Clan = commands.Param(converter=clan_converter)):
+        """
+            Parameters
+            ----------
+            type: Type of reminder you would like to create
+            clan: Use clan tag or select an option from the autocomplete
+            channel: channel to set the join/leave log to
+        """
+        perms = ctx.author.guild_permissions.manage_guild
+        if not perms:
+            embed = disnake.Embed(description="Command requires you to have `Manage Server` permissions.",
+                                  color=disnake.Color.red())
+            return await ctx.send(embed=embed)
 
         results = await self.bot.clan_db.find_one({"$and": [
             {"tag": clan.tag},
@@ -32,166 +49,129 @@ class reminders(commands.Cog):
         if results is None:
             return await ctx.send("This clan is not set up on this server. Use `/addclan` to get started.")
 
+        if type == "Clan Capital":
+            await self.create_clan_capital_reminder(ctx=ctx, channel=channel, clan=clan)
+
+    async def create_clan_capital_reminder(self, ctx: disnake.ApplicationCommandInteraction, channel: disnake.TextChannel, clan: coc.Clan):
+        embed = disnake.Embed(description="**Choose reminder times from list**", color=disnake.Color.green())
+        if ctx.guild.icon is not None:
+            embed.set_thumbnail(url=ctx.guild.icon.url)
+
+        options = [  # the options in your dropdown
+            disnake.SelectOption(label="1 hour remaining", emoji=self.bot.emoji.clock.partial_emoji, value="1 hr"),
+            disnake.SelectOption(label="6 hours remaining", emoji=self.bot.emoji.clock.partial_emoji, value="6 hr"),
+            disnake.SelectOption(label="12 hours remaining", emoji=self.bot.emoji.clock.partial_emoji, value="12 hr"),
+            disnake.SelectOption(label="24 hours remaining", emoji=self.bot.emoji.clock.partial_emoji, value="24 hr")
+        ]
+        select = disnake.ui.Select(
+            options=options,
+            placeholder="Select Reminder Times",  # the placeholder text to show when no options have been chosen
+            min_values=1,  # the minimum number of options a user must select
+            max_values=4,  # the maximum number of options a user can select
+        )
+        dropdown = [disnake.ui.ActionRow(select)]
+        await ctx.send(embed=embed, components=dropdown)
+
+        msg = await ctx.original_message()
+
+        def check(res: disnake.MessageInteraction):
+            return res.message.id == msg.id
+
+        try:
+            res: disnake.MessageInteraction = await self.bot.wait_for("message_interaction", check=check, timeout=600)
+        except:
+            return await msg.edit(components=[])
+
+        await res.response.defer()
+        #delete any previously set ones, so we don't get ones in different channels or times
+        await self.bot.reminders.delete_many({"$and": [
+            {"clan": clan.tag},
+            {"server": ctx.guild.id},
+            {"type": "Clan Capital"}
+        ]})
+        for value in res.values:
+            await self.bot.reminders.insert_one({
+                "server" : ctx.guild.id,
+                "type" : "Clan Capital",
+                "clan" : clan.tag,
+                "channel" : channel.id,
+                "time" : value
+            })
+
+        reminders_created = ", ".join(res.values)
+        embed = disnake.Embed(description=f"**`{reminders_created}` Clan Capital Reminders created for {ctx.guild.name}**", color=disnake.Color.green())
+        if ctx.guild.icon is not None:
+            embed.set_thumbnail(url=ctx.guild.icon.url)
+
+        button = [disnake.ui.ActionRow(disnake.ui.Button(label="Set Custom Text", emoji="✏️", style=disnake.ButtonStyle.green, custom_id="custom_text"))]
+
+        await res.edit_original_message(embed=embed, components=button)
+
+        try:
+            res: disnake.MessageInteraction = await self.bot.wait_for("message_interaction", check=check, timeout=600)
+        except:
+            return await msg.edit(components=[])
+
+        await res.response.send_modal(
+            title="Customize your text",
+            custom_id="customtext-",
+            components=[
+                disnake.ui.TextInput(
+                    label="Extra Custom Text",
+                    placeholder="Extra text to send when reminder is sent (gifs, rules, etc)",
+                    custom_id=f"custom_text",
+                    required=True,
+                    style=disnake.TextInputStyle.paragraph,
+                    max_length=300,
+                )
+            ])
+
+        msg = await res.original_message()
+        await msg.edit(components=[])
+
+        def check(res):
+            return ctx.author.id == res.author.id
+
+        try:
+            modal_inter: disnake.ModalInteraction = await self.bot.wait_for(
+                "modal_submit",
+                check=check,
+                timeout=300,
+            )
+        except:
+            return await msg.edit(components=[])
+
+        await modal_inter.response.defer()
+        custom_text = modal_inter.text_values["custom_text"]
+        ping_reminder = f"{custom_text}\n" \
+                        f"**Example Reminder - Remaining Clan Capital Raids**\n" \
+                        f"2 raids- Linked Player | {ctx.author.mention}\n" \
+                        f"4 raids- Unlinked Player | #playertag"
+        return await modal_inter.edit_original_message(content=ping_reminder)
+
+
+    #async def create_war_reminder(self, ):
 
     @reminder.sub_command(name="remove", description="Remove a reminder set up on the server")
-    async def reminder_remove(self, ctx: disnake.ApplicationCommandInteraction):
+    async def reminder_remove(self, ctx: disnake.ApplicationCommandInteraction, type:str = commands.Param(choices=["Clan Capital", "Clan Games, War", "Inactivity"]), clan: coc.Clan = commands.Param(converter=clan_converter)):
         pass
 
     @reminder.sub_command(name="list", description="Get the list of reminders set up on the server")
     async def reminder_list(self, ctx: disnake.ApplicationCommandInteraction):
         pass
 
+    @reminder_create.autocomplete("clan")
+    async def autocomp_clan(self, ctx: disnake.ApplicationCommandInteraction, query: str):
+        tracked = self.bot.clan_db.find({"server": ctx.guild.id})
+        limit = await self.bot.clan_db.count_documents(filter={"server": ctx.guild.id})
+        clan_list = []
+        for tClan in await tracked.to_list(length=limit):
+            name = tClan.get("name")
+            tag = tClan.get("tag")
+            if query.lower() in name.lower():
+                clan_list.append(f"{name} | {tag}")
 
-    async def clan_capital_reminder(self, time):
-        hour = 4
-        results = self.bot.server_db.find({"topboardchannel": {"$ne" : None}})
-        limit = await self.bot.server_db.count_documents(filter={"topboardchannel": {"$ne" : None}})
-        tasks = []
-        date = self.bot.gen_legend_date()
-        for r in await results.to_list(length=limit):
-            try:
-                serv = r.get("server")
-                channel = r.get("topboardchannel")
-                channel = await self.bot.fetch_channel(channel)
-                g = self.bot.get_guild(serv)
-                limit = 250
-                rankings = []
-                tracked = self.bot.clan_db.find({"server": serv})
-                l = await self.bot.clan_db.count_documents(filter={"server": serv})
-                if l == 0:
-                    continue
-
-                tags = []
-                for clan in await tracked.to_list(length=l):
-                    tag = clan.get("tag")
-                    tags.append(tag)
-
-                async for clan in self.bot.coc_client.get_clans(tags):
-                    for player in clan.members:
-                        try:
-                            playerStats = []
-                            playerStats.append(player.name)
-                            playerStats.append(player.trophies)
-                            playerStats.append(clan.name)
-                            playerStats.append(player.tag)
-                            rankings.append(playerStats)
-                        except:
-                            continue
-
-                if limit > len(rankings):
-                    limit = len(rankings)
-                ranking = sorted(rankings, key=lambda l: l[1], reverse=True)
-
-                embeds = []
-                length = math.ceil(limit / 50)
-                texts = []
-                for e in range(0, length):
-                    rText = ''
-                    max = limit
-                    if (e + 1) * 50 < limit:
-                        max = (e + 1) * 50
-                    for x in range(e * 50, max):
-                        place = str(x + 1) + "."
-                        place = place.ljust(3)
-                        rText += f"\u200e`{place}` \u200e<:trophy:956417881778815016> \u200e{ranking[x][1]} - \u200e{ranking[x][0]} | \u200e{ranking[x][2]}\n"
-
-                    embed = disnake.Embed(title=f"**Top {limit} {g.name} players**",
-                                          description=rText)
-                    texts.append(rText)
-                    if g.icon is not None:
-                        embed.set_thumbnail(url=g.icon.url)
-                    embeds.append(embed)
-                identifier = f"auto_{serv}{date}"
-                if limit > 50:
-                    buttons = disnake.ui.ActionRow()
-                    buttons.append_item(disnake.ui.Button(label="Full Results", emoji=self.bot.emoji.start.partial_emoji,style=disnake.ButtonStyle.grey,custom_id=f"{identifier}"))
-                    await channel.send(embed=embeds[0], components=buttons)
-                else:
-                    await channel.send(embed=embeds[0])
-
-                tasks.append(InsertOne({"identifier": identifier, "text": texts }))
-
-            except (disnake.NotFound, disnake.Forbidden):
-                await self.bot.server_db.update_one({"server": r.get("server")}, {'$set': {"topboardchannel": None}})
-
-        country_results = []
-        results = self.bot.server_db.find({"lbhour": hour+1})
-        limit = await self.bot.server_db.count_documents(filter={"lbhour": hour+1})
-        for r in await results.to_list(length=limit):
-            try:
-                channel = r.get("lbboardChannel")
-                channel =  self.bot.get_channel(channel)
-                serv = r.get("server")
-                g = self.bot.get_guild(serv)
-                country = r.get("country")
-
-                tags = []
-                tracked = self.bot.clan_db.find({"server": g.id})
-                limit = await self.bot.clan_db.count_documents(filter={"server": g.id})
-
-                for clan in await tracked.to_list(length=limit):
-                    tag = clan.get("tag")
-                    tags.append(tag)
-
-                text = ""
-                locations = await self.bot.coc_client.search_locations(limit=None)
-                is_country = (country != "International")
-                country = coc.utils.get(locations, name=country, is_country=is_country)
-                country_names = country.name
-                # print(country.id)
-                rankings = await self.bot.coc_client.get_location_clans(location_id=country.id)
-                # print(rankings)
-
-                x = 1
-                for clan in rankings:
-                    rank = str(x)
-                    rank = rank.ljust(2)
-                    star = ""
-                    if clan.tag in tags:
-                        star = "⭐"
-                    text += f"`\u200e{rank}`🏆`\u200e{clan.points}` \u200e{clan.name}{star}\n"
-                    x += 1
-                    if x == 26:
-                        break
-
-                embed = disnake.Embed(title=f"{country_names} Top 25 Leaderboard",
-                                      description=text,
-                                      color=disnake.Color.green())
-                if g.icon is not None:
-                    embed.set_thumbnail(url=g.icon.url)
-                try:
-                    await channel.send(embed=embed)
-                except:
-                    continue
-            except:
-                pass
-
-        results = await self.bot.autoboards.bulk_write(tasks)
-
-        results = self.bot.server_db.find({"comp_channel": {"$ne" : None}})
-        limit = await self.bot.server_db.count_documents(filter={"comp_channel": {"$ne" : None}})
-        for r in await results.to_list(length=limit):
-            try:
-                channel = r.get("comp_channel")
-                channel = self.bot.get_channel(channel)
-                all_tags = self.bot.erikuh.distinct("player_tag")
-                all_players = await self.bot.get_players(tags=all_tags)
-                serv = r.get("server")
-                guild = self.bot.get_guild(serv)
-                ranking = []
-                for player in all_players:
-                    try:
-                        legend_day = player.legend_day()
-                        ranking.append([player.name, player.trophy_start(), legend_day.attack_sum,
-                                        legend_day.num_attacks.superscript, legend_day.defense_sum,
-                                        legend_day.num_defenses.superscript, player.trophies])
-                    except:
-                        pass
-                ranking = sorted(ranking, key=lambda l: l[6], reverse=True)
-                embeds = await self.create_player_embed(guild, ranking)
-                await channel.send(embed=embeds[0])
-            except:
-                continue
+        return clan_list[0:25]
 
 
 
