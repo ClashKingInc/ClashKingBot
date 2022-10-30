7bf335c8e8d3541c436299a2dabf01be9f351a8d
matthew <matthewanderson3607@gmail.com>
2022-10-29 22:22:04 -0500

    add clan emojis to family donations

--- Family_and_Clans/utils/family.py
+++ Family_and_Clans/utils/family.py
@@ -37,26 +37,37 @@ class getFamily(commands.Cog):
         total_donated = sum(player.donos.donated for player in responses)
         total_received = sum(player.donos.received for player in responses)
 
+        badges = {}
         for player in responses:
             player: MyCustomPlayer
+            if player is None:
+                continue
+            if player.clan is None:
+                continue
             for char in ["`", "*", "_", "~"]:
                 name = player.name.replace(char, "", 10)
             name = emoji.replace_emoji(name, "")
             name = name[:13]
+            name = name.ljust(13)
+            if player.clan.tag not in badges:
+                badge = await self.bot.create_new_badge_emoji(url=player.clan_badge_link())
+                badges[player.clan.tag] = badge
+            else:
+                badge = badges[player.clan.tag]
             donated_text.append(
-                [f"{str(player.donos.donated).ljust(5)} | {str(player.donos.received).ljust(5)} | {name}",
+                [f"{badge}` {str(player.donos.donated).ljust(5)} | {str(player.donos.received).ljust(5)} | \u200e{name}`",
                  player.donos.donated])
             received_text.append(
-                [f"{str(player.donos.received).ljust(5)} | {str(player.donos.donated).ljust(5)} | {name}",
+                [f"{badge}` {str(player.donos.received).ljust(5)} | {str(player.donos.donated).ljust(5)} | \u200e{name}`",
                  player.donos.received])
-            ratio_text.append([f"{str(player.donation_ratio).ljust(5)} | {name}", player.donation_ratio])
+            ratio_text.append([f"{badge}` {str(player.donation_ratio).ljust(5)} | \u200e{name}`", player.donation_ratio])
 
         if type == "donated":
             donated_text = sorted(donated_text, key=lambda l: l[1], reverse=True)
             donated_text = [line[0] for line in donated_text[:50]]
             donated_text = "\n".join(donated_text)
-            donated_text = "DON   | REC   | Name\n" + donated_text
-            donation_embed = disnake.Embed(title=f"**{guild.name} Top 50 Donated**", description=f"```{donated_text}```",
+            donated_text = "<:un:1036115340360429628>` DON   | REC   | Name` \n" + donated_text
+            donation_embed = disnake.Embed(title=f"**{guild.name} Top 50 Donated**", description=f"{donated_text}",
                                            color=disnake.Color.green())
             if guild.icon is not None:
                 icon = guild.icon.url
@@ -69,8 +80,8 @@ class getFamily(commands.Cog):
             received_text = sorted(received_text, key=lambda l: l[1], reverse=True)
             received_text = [line[0] for line in received_text[:50]]
             received_text = "\n".join(received_text)
-            received_text = "REC   | DON   | Name\n" + received_text
-            received_embed = disnake.Embed(title=f"**{guild.name} Top 50 Received**", description=f"```{received_text}```",
+            received_text = "<:un:1036115340360429628>` REC   | DON   | Name`\n" + received_text
+            received_embed = disnake.Embed(title=f"**{guild.name} Top 50 Received**", description=f"{received_text}",
                                            color=disnake.Color.green())
             if guild.icon is not None:
                 icon = guild.icon.url
@@ -83,8 +94,8 @@ class getFamily(commands.Cog):
             ratio_text = sorted(ratio_text, key=lambda l: l[1], reverse=True)
             ratio_text = [line[0] for line in ratio_text[:50]]
             ratio_text = "\n".join(ratio_text)
-            ratio_text = "Ratio | Name\n" + ratio_text
-            ratio_embed = disnake.Embed(title=f"**{guild.name} Top 50 Ratios**", description=f"```{ratio_text}```",
+            ratio_text = "<:un:1036115340360429628>` Ratio | Name`\n" + ratio_text
+            ratio_embed = disnake.Embed(title=f"**{guild.name} Top 50 Ratios**", description=f"{ratio_text}",
                                         color=disnake.Color.green())
             if guild.icon is not None:
                 icon = guild.icon.url
