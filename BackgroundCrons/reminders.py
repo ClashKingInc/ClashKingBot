968b9ed79e9193923fc1a9840710d8b791634b17
matthew <matthewanderson3607@gmail.com>
2022-11-20 17:00:11 -0600

    add 24 hr reminder option

--- BackgroundCrons/reminders.py
+++ BackgroundCrons/reminders.py
@@ -172,12 +172,11 @@ class reminders(commands.Cog, name="Reminders"):
             embed.set_thumbnail(url=ctx.guild.icon.url)
 
         options = []
-        nums = [x * 0.5 for x in range(1, 25)]
+        nums = [x * 0.5 for x in range(1, 25)] + [24]
         for num in nums:
             if num.is_integer():
                 num = int(num)
             options.append(disnake.SelectOption(label=f"{num} hours remaining", emoji=self.bot.emoji.clock.partial_emoji, value=f"{num} hr"))
-        options.append(disnake.SelectOption(label="Remove All", emoji=self.bot.emoji.no.partial_emoji, value="remove"))
         select = disnake.ui.Select(
             options=options,
             placeholder="Select Reminder Times",  # the placeholder text to show when no options have been chosen
