e2b2a0430c217775a870db5edd7986d0f095960b
matthew <matthewanderson3607@gmail.com>
2022-11-25 12:36:24 -0600

    performance improvement join/leave & donoation logs

--- EventHub/join_leave_events.py
+++ EventHub/join_leave_events.py
@@ -17,6 +17,10 @@ class join_leave_events(commands.Cog, name="Clan Join & Leave Events"):
         current_members = event["new_clan"]["memberList"]
         clan_tag = event["new_clan"]["tag"]
 
+        limit = await self.bot.clan_db.count_documents(filter={"tag": f"{clan_tag}"})
+        if limit == 0:
+            return
+
         curr_tags = [member["tag"] for member in current_members]
         prev_tag = [member["tag"] for member in previous_members]
 
@@ -65,7 +69,6 @@ class join_leave_events(commands.Cog, name="Clan Join & Leave Events"):
 
         if donation_text != "" or received_text != "":
             tracked = self.bot.clan_db.find({"tag": f"{clan_tag}"})
-            limit = await self.bot.clan_db.count_documents(filter={"tag": f"{clan_tag}"})
             for cc in await tracked.to_list(length=limit):
                 server = cc.get("server")
                 donolog_channel = cc.get("donolog")
@@ -90,26 +93,25 @@ class join_leave_events(commands.Cog, name="Clan Join & Leave Events"):
         new_tags = list(set(curr_tags).difference(prev_tag))
         left_tags = list(set(prev_tag).difference(curr_tags))
 
+        #if anyone joined or left, fetch clan, get channel then go thru the players & send embeds
         if new_tags or left_tags:
-            async for player in self.bot.coc_client.get_players(new_tags):
-                tracked = self.bot.clan_db.find({"tag": f"{clan_tag}"})
-                limit = await self.bot.clan_db.count_documents(filter={"tag": f"{clan_tag}"})
-                for cc in await tracked.to_list(length=limit):
-                    server = cc.get("server")
-                    joinlog_channel = cc.get("joinlog")
-                    if joinlog_channel is None:
-                        continue
-                    try:
-                        joinlog_channel = await self.bot.fetch_channel(joinlog_channel)
-                    except (disnake.Forbidden, disnake.NotFound):
-                        await self.bot.clan_db.update_one({"$and": [
-                            {"tag": clan_tag},
-                            {"server": server}
-                        ]}, {'$set': {"joinlog": None}})
+            tracked = self.bot.clan_db.find({"tag": f"{clan_tag}"})
+            for cc in await tracked.to_list(length=limit):
+                server = cc.get("server")
+                joinlog_channel = cc.get("joinlog")
+                if joinlog_channel is None:
+                    continue
+                try:
+                    joinlog_channel = await self.bot.fetch_channel(joinlog_channel)
+                except (disnake.Forbidden, disnake.NotFound):
+                    await self.bot.clan_db.update_one({"$and": [
+                        {"tag": clan_tag},
+                        {"server": server}
+                    ]}, {'$set': {"joinlog": None}})
 
-                    if joinlog_channel is None:
-                        continue
-
+                if joinlog_channel is None:
+                    continue
+                async for player in self.bot.coc_client.get_players(new_tags):
                     hero = heros(player)
                     pets = heroPets(player)
                     if hero is None:
@@ -132,29 +134,7 @@ class join_leave_events(commands.Cog, name="Clan Join & Leave Events"):
                     except:
                         continue
 
-            async for player in self.bot.coc_client.get_players(left_tags):
-                tracked = self.bot.clan_db.find({"tag": f"{clan_tag}"})
-                limit = await self.bot.clan_db.count_documents(filter={"tag": f"{clan_tag}"})
-                for cc in await tracked.to_list(length=limit):
-                    server = cc.get("server")
-                    try:
-                        server = await self.bot.fetch_guild(server)
-                    except:
-                        continue
-                    joinlog_channel = cc.get("joinlog")
-                    if joinlog_channel is None:
-                        continue
-                    try:
-                        joinlog_channel = await server.fetch_channel(joinlog_channel)
-                    except (disnake.Forbidden, disnake.NotFound):
-                        await self.bot.clan_db.update_one({"$and": [
-                            {"tag": clan_tag},
-                            {"server": server.id}
-                        ]}, {'$set': {"joinlog": None}})
-
-                    if joinlog_channel is None:
-                        continue
-
+                async for player in self.bot.coc_client.get_players(left_tags):
                     hero = heros(player)
                     pets = heroPets(player)
                     if hero is None:
@@ -173,18 +153,19 @@ class join_leave_events(commands.Cog, name="Clan Join & Leave Events"):
                                           , color=disnake.Color.red())
 
                     if player.clan is not None:
-                        #rclan = await self.bot.getClan(player.clan.tag)
+                        # rclan = await self.bot.getClan(player.clan.tag)
                         embed.set_footer(icon_url=player.clan.badge.url,
                                          text=f"Left {event['new_clan']['name']} [{event['new_clan']['members']}/50] and Joined {player.clan.name}")
                     else:
                         embed.set_footer(icon_url=event["new_clan"]["badgeUrls"]["large"],
                                          text=f"Left {event['new_clan']['name']} [{event['new_clan']['members']}/50]")
 
-
                     try:
                         await joinlog_channel.send(embed=embed)
                     except:
                         continue
+
+
 
 
 
