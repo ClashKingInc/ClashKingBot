50c99a248bfa03a6056c2c48672feb78b28acace
matthew <matthewanderson3607@gmail.com>
2022-11-28 16:58:52 -0600

    test global chat revamp

--- global_chat.py
+++ global_chat.py
@@ -195,11 +195,11 @@ class GlobalChat(commands.Cog, name="Global Chat"):
                         glob_webhook = await glob_channel.create_webhook(name="Global Chat", reason="Global Chat")
                     except:
                         return
-                self.bot.global_webhooks[channel] = glob_webhook.id
+                self.bot.global_webhooks[channel] = glob_webhook
                 await send_web(glob_webhook, embed_)
             else:
                 try:
-                    webhook = await self.bot.fetch_webhook(self.bot.global_webhooks[channel])
+                    webhook = self.bot.global_webhooks[channel]
                     await send_web(webhook, embed_)
                 except:
                     self.bot.global_webhooks[channel] = ""
