3584f3cc2a0feabe778443e113e14a334af5abf1
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-03-13 00:28:39 -0500

    stuff

--- utils/General.py
+++ utils/General.py
@@ -1,4 +1,9 @@
 SUPER_SCRIPTS=["⁰","¹","²","³","⁴","⁵","⁶", "⁷","⁸", "⁹"]
+from coc import utils
+from datetime import datetime
+import datetime as dt
+import pytz
+utc = pytz.utc
 
 async def fetch(url, session):
     async with session.get(url) as response:
@@ -11,3 +16,166 @@ def create_superscript(num):
         new_num += SUPER_SCRIPTS[d]
 
     return new_num
+
+async def calculate_time(type):
+    text = ""
+    now = datetime.utcnow().replace(tzinfo=utc)
+    year = now.year
+    month = now.month
+    day = now.day
+    hour = now.hour
+    if type == "CWL":
+        is_cwl = True
+        if day == 1:
+            first = datetime(year, month, 1, hour=8, tzinfo=utc)
+        else:
+            if month + 1 == 13:
+                next_month = 1
+                next_year = year + 1
+            else:
+                next_month = month + 1
+                next_year = year
+            first = datetime(next_year, next_month, 1, hour=8, tzinfo=utc)
+        end = datetime(year, month, 11, hour=8, tzinfo=utc)
+        if (day >= 1 and day <= 10):
+            if (day == 1 and hour < 8) or (day == 11 and hour >= 8):
+                is_cwl = False
+            else:
+                is_cwl = True
+        else:
+            is_cwl = False
+
+        if is_cwl:
+            time_left = end - now
+            secs = time_left.total_seconds()
+            days, secs = divmod(secs, secs_per_day := 60 * 60 * 24)
+            hrs, secs = divmod(secs, secs_per_hr := 60 * 60)
+            mins, secs = divmod(secs, secs_per_min := 60)
+            if int(days) == 0:
+                text = f"ends {int(hrs)}H {int(mins)}M"
+                if int(hrs) == 0:
+                    text = f"ends in {int(mins)}M"
+            else:
+                text = f"ends {int(days)}D {int(hrs)}H"
+        else:
+            time_left = first - now
+            secs = time_left.total_seconds()
+            days, secs = divmod(secs, secs_per_day := 60 * 60 * 24)
+            hrs, secs = divmod(secs, secs_per_hr := 60 * 60)
+            mins, secs = divmod(secs, secs_per_min := 60)
+            if int(days) == 0:
+                text = f"in {int(hrs)}H {int(mins)}M"
+                if int(hrs) == 0:
+                    text = f"in {int(mins)}M"
+            else:
+                text = f"in {int(days)}D {int(hrs)}H"
+
+    elif type == "Clan Games":
+        is_games = True
+        first = datetime(year, month, 22, hour=8, tzinfo=utc)
+        end = datetime(year, month, 28, hour=8, tzinfo=utc)
+        if (day >= 22 and day <= 28):
+            if (day == 22 and hour < 8) or (day == 28 and hour >= 8):
+                is_games = False
+            else:
+                is_games = True
+        else:
+            is_games = False
+
+        if day == 28 and hour >= 8:
+            if month + 1 == 13:
+                next_month = 1
+                year += 1
+            else:
+                next_month = month + 1
+            first = datetime(year, next_month, 22, hour=8, tzinfo=utc)
+
+        if day >= 29:
+            if month + 1 == 13:
+                next_month = 1
+                year += 1
+            else:
+                next_month = month + 1
+            first = datetime(year, next_month, 22, hour=8, tzinfo=utc)
+
+        if is_games:
+            time_left = end - now
+            secs = time_left.total_seconds()
+            days, secs = divmod(secs, secs_per_day := 60 * 60 * 24)
+            hrs, secs = divmod(secs, secs_per_hr := 60 * 60)
+            mins, secs = divmod(secs, secs_per_min := 60)
+            if int(days) == 0:
+                text = f"ends {int(hrs)}H {int(mins)}M"
+                if int(hrs) == 0:
+                    text = f"ends in {int(mins)}M"
+            else:
+                text = f"ends {int(days)}D {int(hrs)}H"
+        else:
+            time_left = first - now
+            secs = time_left.total_seconds()
+            days, secs = divmod(secs, secs_per_day := 60 * 60 * 24)
+            hrs, secs = divmod(secs, secs_per_hr := 60 * 60)
+            mins, secs = divmod(secs, secs_per_min := 60)
+            if int(days) == 0:
+                text = f"in {int(hrs)}H {int(mins)}M"
+                if int(hrs) == 0:
+                    text = f"in {int(mins)}M"
+            else:
+                text = f"in {int(days)}D {int(hrs)}H"
+
+    elif type == "Raid Weekend":
+
+        now = datetime.utcnow().replace(tzinfo=utc)
+        current_dayofweek = now.weekday()
+        if (current_dayofweek == 4 and now.hour >= 7) or (current_dayofweek == 5) or (current_dayofweek == 6) or (
+                current_dayofweek == 0 and now.hour < 7):
+            if current_dayofweek == 0:
+                current_dayofweek = 7
+            is_raids = True
+        else:
+            is_raids = False
+
+        if is_raids:
+            end = datetime(year, month, day, hour=7, tzinfo=utc) + dt.timedelta(days= (7 - current_dayofweek))
+            time_left = end - now
+            secs = time_left.total_seconds()
+            days, secs = divmod(secs, secs_per_day := 60 * 60 * 24)
+            hrs, secs = divmod(secs, secs_per_hr := 60 * 60)
+            mins, secs = divmod(secs, secs_per_min := 60)
+            if int(days) == 0:
+                text = f"end {int(hrs)}H {int(mins)}M"
+                if int(hrs) == 0:
+                    text = f"end in {int(mins)}M"
+            else:
+                text = f"end {int(days)}D {int(hrs)}H"
+        else:
+            first = datetime(year, month, day, hour=7, tzinfo=utc) + dt.timedelta(days=(4 - current_dayofweek))
+            time_left = first - now
+            secs = time_left.total_seconds()
+            days, secs = divmod(secs, secs_per_day := 60 * 60 * 24)
+            hrs, secs = divmod(secs, secs_per_hr := 60 * 60)
+            mins, secs = divmod(secs, secs_per_min := 60)
+            if int(days) == 0:
+                text = f"in {int(hrs)}H {int(mins)}M"
+                if int(hrs) == 0:
+                    text = f"in {int(mins)}M"
+            else:
+                text = f"in {int(days)}D {int(hrs)}H"
+
+    elif type == "EOS":
+        end = utils.get_season_end().replace(tzinfo=utc)
+        time_left = end - now
+        secs = time_left.total_seconds()
+        days, secs = divmod(secs, secs_per_day := 60 * 60 * 24)
+        hrs, secs = divmod(secs, secs_per_hr := 60 * 60)
+        mins, secs = divmod(secs, secs_per_min := 60)
+
+        if int(days) == 0:
+            text = f"in {int(hrs)}H {int(mins)}M"
+            if int(hrs) == 0:
+                text = f"in {int(mins)}M"
+        else:
+            text = f"in {int(days)}D {int(hrs)}H "
+
+
+    return text
