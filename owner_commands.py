c46d63b402206f94b93ba0e830c2e620fbfef4a3
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-07-12 01:00:34 -0500

    ClashKing v4

--- owner_commands.py
+++ owner_commands.py
@@ -79,7 +79,7 @@ class OwnerCommands(commands.Cog):
 
             try:
                 webhook = await self.bot.getch_webhook(log.webhook)
-                if isinstance(webhook, disnake.ForumChannel) and log.thread is None:
+                if isinstance(webhook.channel, disnake.ForumChannel) and log.thread is None:
                     raise disnake.Forbidden
                 if webhook.user.id != self.bot.user.id:
                     webhook = await get_webhook_for_channel(bot=self.bot, channel=webhook.channel)
