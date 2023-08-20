48e6a4b5fafaa79981da3698a889cf4de925d5f3
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-08-20 16:43:50 -0500

    ClashKing v4

--- other.py
+++ other.py
@@ -168,7 +168,7 @@ class misc(commands.Cog, name="Other"):
             if len(message_text) + len(f"{message.author.display_name} said: {message.content}\n") > 4000:
                 continue
             message_text += f"{message.author.display_name} said: {message.content}\n"
-        magicbot = ChatBot(f"Summarize, aka TLDR this conversation")
+        magicbot = ChatBot(f"Summarize, aka TLDR, this conversation using as few bullet points as possible")
         message = magicbot(message_text)
         content = f"Summary, {num_messages} messages, #{channel.name}:\n {message}"
         await ctx.edit_original_message(content=content[:2000])
