632ddab1818ef9ba0cff5c767db4864452789c43
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-08-24 21:02:15 -0500

    ClashKing v4

--- Tracking/clan_verifier.py
+++ Tracking/clan_verifier.py
@@ -112,6 +112,9 @@ class League(Struct):
 class ClanCapital(Struct):
     capitalHallLevel: Optional[int] = 0
 
+class Badge(Struct):
+    large: Optional[str] = ""
+
 class Clan(Struct):
     name: str
     tag: str
@@ -122,6 +125,7 @@ class Clan(Struct):
     warWinStreak: int
     warWins: int
     clanCapital: ClanCapital
+    badgeUrls: Badge
 
 async def broadcast(keys):
 
@@ -173,6 +177,7 @@ async def broadcast(keys):
                         changes.append(UpdateOne({"tag": clan.tag},
                                                       {"$set":
                                                            {"name": clan.name,
+                                                            "badge" : clan.badge.large,
                                                             "members" : clan.members,
                                                             "clanCapitalPoints" : clan.clanCapitalPoints,
                                                             "capitalLeague" : clan.capitalLeague.name,
