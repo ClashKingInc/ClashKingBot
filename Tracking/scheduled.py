4ddfc76a73e9bdd026dacc52142ed5cd97baa8e8
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-09-16 13:35:32 -0500

    ClashKing v4

--- Tracking/scheduled.py
+++ Tracking/scheduled.py
@@ -287,7 +287,7 @@ async def store_cwl():
 
 
 #@scheduler.scheduled_job("cron", day="12", hour="1-22", minute=10)
-@scheduler.scheduled_job("cron", day="16", hour="18", minute=34)
+@scheduler.scheduled_job("cron", day="16", hour="18", minute=36)
 async def store_rounds():
     season = gen_season_date()
     pipeline = [{"$match": {"data.season": season}},
