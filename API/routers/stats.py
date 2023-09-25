a5d76838f12404ce5121c25c1b7ff3f36cfd1cc8
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-09-25 01:26:48 -0500

    ClashKing v4

--- API/routers/stats.py
+++ API/routers/stats.py
@@ -7,9 +7,10 @@ from fastapi_cache.decorator import cache
 from typing import List, Annotated
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
-from .utils import fix_tag, capital, leagues, clan_cache_db, clan_stats, basic_clan, clan_history, attack_db, clans_db, gen_season_date, player_stats_db, rankings
-from bson.objectid import ObjectId
+from .utils import fix_tag, capital, leagues, clan_cache_db, clan_stats, basic_clan, clan_history, attack_db, clans_db, gen_season_date, player_stats_db, rankings, player_history, is_games, gen_games_season
 from statistics import mean, median
+from datetime import datetime
+from pytz import utc
 
 limiter = Limiter(key_func=get_remote_address)
 router = APIRouter(tags=["Stat Endpoints"])
@@ -191,6 +192,142 @@ async def activity(request: Request, response: Response,
     return {"items" : new_data, "totals" : totals,
             "metadata" : {"sort_order" : ("descending" if descending else "ascending"), "sort_field" : sort_field, "season" : season}}
 
+
+@router.get("/clan-games",
+         name="Clan Game Stats")
+@cache(expire=300)
+@limiter.limit("30/second")
+async def clan_games(request: Request, response: Response,
+                           players: Annotated[List[str], Query(max_length=50)]=None,
+                           clans: Annotated[List[str], Query(max_length=25)]=None,
+                           server: int =None,
+                           sort_field: str = "points",
+                           townhalls: Annotated[List[str], Query(max_length=15)]=None,
+                           season: str = None,
+                           tied_only: bool = True,
+                           descending: bool = True,
+                           limit: int = 50):
+    limit = min(limit, 500)
+    season = gen_games_season() if season is None else season
+    if server:
+        clans = await clans_db.distinct("tag", filter={"server" : server})
+    check_time = int(datetime.now().timestamp())
+    current_season = gen_season_date()
+    if season != current_season:
+        split_season = season.split("-")
+        check_time  = int(datetime(int(split_season[0]), int(split_season[1]), 28, hour=8, tzinfo=utc).timestamp())
+
+    year = int(season[:4])
+    month = int(season[-2:])
+    next_month = month + 1
+    if month == 12:
+        next_month = 1
+        year += 1
+    start = datetime(year, month, 1)
+    end = datetime(year, next_month, 1)
+    new_data = []
+    if players:
+        pipeline = [
+            {"$match": {"$and": [{"tag": {"$in": [fix_tag(player) for player in players]}}, {"type": "Games Champion"},
+                                 {"time": {"$gte": start.timestamp()}},
+                                 {"time": {"$lte": end.timestamp()}}]}},
+            {"$sort": {"tag": 1, "time": 1}},
+            {"$group": {"_id": "$tag", "first": {"$first": "$time"}, "last": {"$last": "$time"}}}
+        ]
+        results: List[dict] = await player_history.aggregate(pipeline).to_list(length=None)
+        member_stat_dict = {}
+        for m in results:
+            member_stat_dict[m["_id"]] = {"first": m["first"], "last": m["last"]}
+        stat_results = await player_stats_db.find({"tag" : {"$in" : [fix_tag(player) for player in players]}},
+                                                  {"tag" : 1, "name" : 1, "clan_games" : 1, "townhall" : 1}).to_list(length=None)
+        player_struct = {m.get("tag") : {"tag" : m.get("tag"), "name" : m.get("name"), "rank" : 0,
+                                         "points" : m.get("clan_games", {}).get(season, {}).get("points", 0),
+                                         "time_taken" : 0, "townhall" : m.get("townhall", 0)} for m in stat_results}
+        for tag, value in player_struct.items():
+            points = value.get("points")
+            stats = member_stat_dict.get(tag)
+            if stats is not None:
+                if points < 4000:
+                    stats["last"] = check_time
+                first_time = datetime.fromtimestamp(stats["first"])
+                last_time = datetime.fromtimestamp(stats["last"])
+                player_struct[tag]["time_taken"] = (last_time - first_time).seconds
+        new_data = list(player_struct.values())
+
+    elif clans:
+        clan_members = await basic_clan.find({"tag" : {"$in" : [fix_tag(clan) for clan in clans]}}).to_list(length=None)
+        member_tags = []
+        member_to_name = {}
+        for c in clan_members:
+            member_list = c.get("memberList", [])
+            member_tags += [m.get("tag") for m in member_list]
+            for m in member_list:
+                member_to_name[m.get("tag")] = m.get("name")
+        pipeline = [
+            {"$match": {"$and": [{"tag": {"$in": member_tags}}, {"type": "Games Champion"},
+                                 {"time": {"$gte": start.timestamp()}},
+                                 {"time": {"$lte": end.timestamp()}}]}},
+            {"$sort": {"tag": 1, "time": 1}},
+            {"$group": {"_id": "$tag", "first": {"$first": "$time"}, "last": {"$last": "$time"}}}
+        ]
+        results: List[dict] = await player_history.aggregate(pipeline).to_list(length=None)
+        member_stat_dict = {}
+        for m in results:
+            member_stat_dict[m["_id"]] = {"first": m["first"], "last": m["last"]}
+        stat_results = await clan_stats.find({"tag": {"$in" : [fix_tag(clan) for clan in clans]}}).to_list(length=None)
+        player_struct = {tag : {"tag" : tag, "name" : member_to_name.get(tag), "rank" : 0, "points" : 0, "time_taken" : 0, "townhall" : 0} for tag in member_tags}
+        member_data = await player_stats_db.find({"tag" : {"$in" : member_tags}}, {"tag" : 1, "name" : 1, "clan_games" : 1, "townhall" : 1}).to_list(length=None)
+        for member in member_data:
+            if not tied_only:
+                player_struct[member.get("tag")]["points"] = member.get("clan_games", {}).get(season, {}).get("points", 0)
+            player_struct[member.get("tag")]["townhall"] = member.get("townhall", None)
+        if tied_only:
+            for result in stat_results:
+                this_season = result.get(season)
+                if this_season is not None:
+                    for tag in member_tags:
+                        this_player = this_season.get(tag)
+                        if this_player is None:
+                            continue
+                        player_struct[tag]["points"] += this_player.get("clan_games", 0)
+
+        for tag, value in player_struct.items():
+            points = value.get("points")
+            if points == 0:
+                continue
+            stats = member_stat_dict.get(tag)
+            if stats is not None:
+                if points < 4000:
+                    stats["last"] = check_time
+                first_time = datetime.fromtimestamp(stats["first"])
+                last_time = datetime.fromtimestamp(stats["last"])
+                player_struct[tag]["time_taken"] = (last_time - first_time).seconds
+        new_data = list(player_struct.values())
+
+
+    totals = {"points" : 0, "average_points" : [], "average_townhall" : []}
+    for data in new_data:
+        totals["points"] += data.get("points")
+        if data.get("townhall"):
+            totals["average_points"].append(data.get("points"))
+            totals["average_townhall"].append(data.get("townhall"))
+    totals["average_points"] = round(mean(totals.get("average_points")), 2)
+    totals["average_townhall"] = round(mean(totals.get("average_townhall")), 2)
+
+    if townhalls:
+        townhalls = [int(th) for th in townhalls if th.isnumeric()]
+        new_data = [data for data in new_data if data.get("townhall") in townhalls]
+
+    if sort_field == "time_taken":
+        new_data = [data for data in new_data if data.get("points") != 0]
+        new_data = sorted(new_data, key=lambda x: (x.get("points") >= 4000, -x.get(sort_field)), reverse=descending)[:limit]
+    else:
+     new_data = sorted(new_data, key=lambda x: x.get(sort_field), reverse=descending)[:limit]
+    for count, data in enumerate(new_data, 1):
+        data["rank"] = count
+
+    return {"items" : new_data, "totals" : totals,
+            "metadata" : {"sort_order" : ("descending" if descending else "ascending"), "sort_field" : sort_field, "season" : season}}
 
 
 
