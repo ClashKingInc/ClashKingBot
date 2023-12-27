83e70ffc85c8640988d8c1420aa97b422134af58
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-12-27 17:48:36 -0600

    ClashKing v4

--- Tracking/clan_verifier.py
+++ Tracking/clan_verifier.py
@@ -174,7 +174,7 @@ async def broadcast(keys):
                                 {"$setWindowFields": {
                                     "sortBy": {"trophies": -1},
                                     "output": {
-                                        "rank": {"$rank": {}}
+                                        "rank": {"$denseRank": {}}
                                     }
                                 }},
                                 {"$out": {"db": "new_looper", "coll": "legend_rankings"}}
