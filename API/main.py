66c7ea8df83ae2e23e0352bedec63a79bd09fada
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-01-03 15:42:29 -0600

    ClashKing v4

--- API/main.py
+++ API/main.py
@@ -25,7 +25,7 @@ LOCAL = False
 load_dotenv()
 
 limiter = Limiter(key_func=get_remote_address)
-app = FastAPI()
+app = FastAPI(servers=None)
 
 
 async def catch_exceptions_middleware(request: Request, call_next):
