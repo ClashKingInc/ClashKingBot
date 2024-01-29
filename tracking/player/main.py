8b62084f7ce69990371b834e980f5679affa0561
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-01-29 01:03:39 -0600

    ClashKing v4

--- tracking/player/main.py
+++ tracking/player/main.py
@@ -8,15 +8,14 @@ from .tracking import main
 from ..utils import create_keys
 
 
-if __name__ == '__main__':
-    config = Config()
-    loop = asyncio.get_event_loop()
+config = Config()
+loop = asyncio.get_event_loop()
 
-    stats_mongo_client = motor.motor_asyncio.AsyncIOMotorClient(config.stats_mongodb)
-    static_mongo_client = motor.motor_asyncio.AsyncIOMotorClient(config.static_mongodb)
+stats_mongo_client = motor.motor_asyncio.AsyncIOMotorClient(config.stats_mongodb)
+static_mongo_client = motor.motor_asyncio.AsyncIOMotorClient(config.static_mongodb)
 
-    redis_host = redis.Redis(host=config.redis_ip, port=6379, db=0, password=config.redis_pw, decode_responses=False, max_connections=2500)
-    keys = create_keys([f"apiclashofclans+test{x}@gmail.com" for x in range(config.min_coc_email, config.max_coc_email + 1)], [config.coc_password] * config.max_coc_email)
-    keys = deque(keys)
-    loop.create_task(main(keys=keys, cache=redis_host, stats_mongo_client=stats_mongo_client, static_mongo_client=static_mongo_client, config=config))
-    loop.run_forever()
+redis_host = redis.Redis(host=config.redis_ip, port=6379, db=0, password=config.redis_pw, decode_responses=False, max_connections=2500)
+keys = create_keys([f"apiclashofclans+test{x}@gmail.com" for x in range(config.min_coc_email, config.max_coc_email + 1)], [config.coc_password] * config.max_coc_email)
+keys = deque(keys)
+loop.create_task(main(keys=keys, cache=redis_host, stats_mongo_client=stats_mongo_client, static_mongo_client=static_mongo_client, config=config))
+loop.run_forever()
