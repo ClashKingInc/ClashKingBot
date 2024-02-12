4acf84b4b0a797db7cef1c72136fad3f32ff42e2
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-02-12 17:42:24 -0600

    ClashKing v4

--- classes/DatabaseClient/client.py
+++ classes/DatabaseClient/client.py
@@ -31,6 +31,30 @@ class BaseClient():
             {"$lookup": {"from": "clans", "localField": "server", "foreignField": "server", "as": "clans"}},
         ]
         data = await self.bot.server_db.aggregate(pipeline).to_list(length=1)
+        if not data:
+            await self.bot.server_db.insert_one({
+                "server": server_id,
+                "banlist": None,
+                "greeting": None,
+                "cwlcount": None,
+                "topboardchannel": None,
+                "tophour": None,
+                "lbboardChannel": None,
+                "lbhour": None,
+            })
+            pipeline = [
+                {"$match": {"server": server_id}},
+                {"$lookup": {"from": "legendleagueroles", "localField": "server", "foreignField": "server", "as": "eval.league_roles"}},
+                {"$lookup": {"from": "evalignore", "localField": "server", "foreignField": "server", "as": "eval.ignored_roles"}},
+                {"$lookup": {"from": "generalrole", "localField": "server", "foreignField": "server", "as": "eval.family_roles"}},
+                {"$lookup": {"from": "linkrole", "localField": "server", "foreignField": "server", "as": "eval.not_family_roles"}},
+                {"$lookup": {"from": "familyexclusiveroles", "localField": "server", "foreignField": "server", "as": "eval.only_family_roles"}},
+                {"$lookup": {"from": "townhallroles", "localField": "server", "foreignField": "server", "as": "eval.townhall_roles"}},
+                {"$lookup": {"from": "builderhallroles", "localField": "server", "foreignField": "server", "as": "eval.builderhall_roles"}},
+                {"$lookup": {"from": "builderleagueroles", "localField": "server", "foreignField": "server", "as": "eval.builder_league_roles"}},
+                {"$lookup": {"from": "clans", "localField": "server", "foreignField": "server", "as": "clans"}},
+            ]
+            data = await self.bot.server_db.aggregate(pipeline).to_list(length=1)
         return DatabaseServer(bot=self.bot, data=data[0])
 
 
