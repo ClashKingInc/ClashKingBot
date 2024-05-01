2e1aaed9ab850078877949ec5fe379a4c9135d79
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-05-01 06:40:00 -0500

    fix: help command, add bot

--- commands/help/commands.py
+++ commands/help/commands.py
@@ -27,7 +27,7 @@ class HelpCommands(commands.Cog, name="Help"):
         await ctx.response.defer()
         embed_color = await self.bot.ck_client.get_server_embed_color(server_id=ctx.guild_id)
 
-        all_commands: Dict[str, List[disnake.ext.commands.InvokableSlashCommand]] = get_all_commands()
+        all_commands: Dict[str, List[disnake.ext.commands.InvokableSlashCommand]] = get_all_commands(bot=self.bot)
         if category:
             all_commands = {category : all_commands.get(category)}
 
