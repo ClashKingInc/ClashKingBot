bb5d664088519b940940f95b91bf6ffa30ae7551
MagicTheDev <82341152+MagicTheDev@users.noreply.github.com>
2024-05-01 12:30:21 -0500

    Update commands.py

--- commands/embeds/commands.py
+++ commands/embeds/commands.py
@@ -45,7 +45,7 @@ class Embeds(commands.Cog):
             raise MessageException("No embed with that name found on this server")
         if data_link is None:
             encoding = encoded_data(data=lookup.get("data"))
-            shortened_url = await shorten_link(url=f"https://glitchii.github.io/embedbuilder/?data={encoding}&verified&username=ClashKing&avatar={self.bot.user.avatar.url}")
+            shortened_url = await shorten_link(url=f"https://discohook.org/?data={encoding})
             buttons = disnake.ui.ActionRow(
                 disnake.ui.Button(label="Edit Embed",
                                   url=shortened_url,
