422b2759219455c9f428540ef528f9a229481a0a
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-06-13 21:37:33 -0500

    autorefresh, embed, ban log, and countdown bug fixes

--- background/logs/bans.py
+++ background/logs/bans.py
@@ -20,41 +20,50 @@ class BanEvents(commands.Cog):
 
         if members_joined:
             results = await self.bot.banlist.find({"VillageTag": {"$in" :  [m.tag for m in members_joined]}}).to_list(length=None)
-            if results:
-                for cc in await self.bot.clan_db.find({"tag": f"{clan.tag}"}).to_list(length=None):
-                    db_clan = DatabaseClan(bot=self.bot, data=cc)
-                    if db_clan.server_id not in self.bot.OUR_GUILDS:
-                        continue
+            #members that have ban results
+            for result in results:
+                #go thru each banned person, and find out what server they are banned on
+                ban_server_id = result.get("server")
+                if ban_server_id not in self.bot.OUR_GUILDS:
+                    continue
+
+                #find the clan + server combination
+                clan_result = await self.bot.clan_db.find_one({"$and": [
+                    {"tag": clan.tag},
+                    {"server": ban_server_id}
+                ]})
+                if clan_result:
+                    db_clan = DatabaseClan(bot=self.bot, data=clan_result)
+
                     server = await self.bot.getch_guild(db_clan.server_id)
                     if server is None:
                         continue
 
-                    for result in results:
-                        member = coc.utils.get(members_joined, tag=result.get("VillageTag"))
+                    member = coc.utils.get(members_joined, tag=result.get("VillageTag"))
 
-                        notes = result.get("Notes")
-                        if notes == "":
-                            notes = "No Reason Given"
-                        date = result.get("DateCreated")[:10]
+                    notes = result.get("Notes")
+                    if notes == "":
+                        notes = "No Reason Given"
+                    date = result.get("DateCreated")[:10]
 
-                        role = f"<@&{db_clan.member_role}>"
-                        embed = disnake.Embed(
-                            description=f"{role}\n[WARNING! BANNED PLAYER {member.name} JOINED]({member.share_link})",
-                            color=disnake.Color.green())
-                        embed.add_field(name="Banned Player.",
-                                        value=f"Player {member.name} [{member.tag}] has joined {clan.name} and is on the {server.name} BAN list!\n\n"
-                                              f"Banned on: {date}\nReason: {notes}")
-                        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/843624785560993833/932701461614313562/2EdQ9Cx.png")
+                    role = f"<@&{db_clan.member_role}>"
+                    embed = disnake.Embed(
+                        description=f"{role}\n[WARNING! BANNED PLAYER {member.name} JOINED]({member.share_link})",
+                        color=disnake.Color.green())
+                    embed.add_field(name="Banned Player.",
+                                    value=f"Player {member.name} [{member.tag}] has joined {clan.name} and is on the {server.name} BAN list!\n\n"
+                                          f"Banned on: {date}\nReason: {notes}")
+                    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/843624785560993833/932701461614313562/2EdQ9Cx.png")
 
-                        try:
-                            channel = await self.bot.getch_channel(channel_id=db_clan.ban_alert_channel or db_clan.clan_channel)
-                            await channel.send(content=role, embed=embed)
-                        except (disnake.NotFound, disnake.Forbidden):
-                            if db_clan.ban_alert_channel is None:
-                                await db_clan.set_clan_channel(id=None)
-                            else:
-                                await db_clan.set_ban_alert_channel(id=None)
-                            continue
+                    try:
+                        channel = await self.bot.getch_channel(channel_id=db_clan.ban_alert_channel or db_clan.clan_channel)
+                        await channel.send(content=role, embed=embed)
+                    except (disnake.NotFound, disnake.Forbidden):
+                        if db_clan.ban_alert_channel is None:
+                            await db_clan.set_clan_channel(id=None)
+                        else:
+                            await db_clan.set_ban_alert_channel(id=None)
+                        continue
 
 
 def setup(bot: CustomClient):
